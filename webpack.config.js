b80116fd7ed4036ea0359940bfe3a27d90e9db83
Hotpause <ashutoshagarwal1006@gmail.com>
2024-04-08 07:53:08 +0530

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'TODO ASHU',
+            filename: 'index.html',
+            inject: 'body',
+        })
+    ]
+};
