7bdfe4fa8d50b8bf25d7e24650df7454413f5495
Hotpause <ashutoshagarwal1006@gmail.com>
2024-04-08 19:27:42 +0530

    Add 2 modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './style.css';
+import project from './project';
+import todo from './todo';
 
-console.log("hellooo jiiii");
+console.log("Hello World");
+
+
+const project1 = new project('ashu');
+const todo1 = new todo('milk', 'get milk', '', '1');
+project1.addtodo(todo1);
+
+console.log(project1.name);
+
