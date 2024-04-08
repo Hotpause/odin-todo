a348e482aa441bdf376aa9590255f523582b9b48
Hotpause <ashutoshagarwal1006@gmail.com>
2024-04-08 19:59:18 +0530

    Help Commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import './style.css';
 import project from './project';
 import todo from './todo';
 
-console.log("Hello World");
+console.log("Hello Worlddd");
 
 
 const project1 = new project('ashu');
 const todo1 = new todo('milk', 'get milk', '', '1');
 project1.addtodo(todo1);
 
-console.log(project1.name);
+console.log(project1);
 
